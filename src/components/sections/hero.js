dc08a5f2ec6ccabb9af5556b89035796609afb1f
Jaskirat Singh <jaskirat1999@gmail.com>
2024-12-24 08:21:47 -0500

    updated cover image and hero source code

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,28 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Jaskirat Singh.</h2>;
+  const three = <h3 className="big-heading">I build things 💻.</h3>;
+  // TODO: Add AI related link
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
+        I’m a software engineer specializing in building full stack applications. Currently, I’m focused on developing seamless {' '} 
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+          LLM integrations ✨
         </a>
-        .
+        {' '}into existing solutions.
       </p>
     </>
   );
+  // TODO: Update this with something cool
   const five = (
     <a
       className="email-link"
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my [insert cool thing]!
     </a>
   );
 
