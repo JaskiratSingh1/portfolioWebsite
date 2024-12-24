678a781d8895ae134b0939b1e3dddc875f18396d
Jaskirat Singh <jaskirat1999@gmail.com>
2024-12-24 07:35:25 -0500

    updated configs

--- src/components/head.js
+++ src/components/head.js
@@ -59,6 +59,7 @@ const Head = ({ title, description, image }) => {
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
 
+      // TODO: Update Google Site Verification
       <meta name="google-site-verification" content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk" />
     </Helmet>
   );
